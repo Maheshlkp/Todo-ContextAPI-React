15f57bcbeab8665b5c8bea4a1200166d17ded8a7
Mahesh <Mahesh.lkp1234@gmail.com>
2020-10-09 00:19:48 -0500

    context api with action and reducer

---
+++ todo-contextapi/src/Components/Todos.js
@@ -0,0 +1,27 @@
+import React, { useContext } from 'react';
+import {FaCheckDouble} from "react-icons/fa"
+import {ListGroup, ListGroupItem} from "reactstrap";
+import {TodoContext} from "../context/TodoContext";
+import {REMOVE_TODO} from "../context/action.types";
+
+
+const Todos = () =>{
+    const {todos, dispatch} = useContext(TodoContext);
+    return(
+        <ListGroup className="mt-5 mb-2 items">
+            {todos.map(todo =>(
+                <ListGroupItem key={todo.id}>
+                    {todo.toString}
+                    <span className="float-right" onClick={()=>{
+                        dispatch({
+                            type: REMOVE_TODO,
+                        payload: todo.id
+                        }) 
+                    }}><FaCheckDouble></FaCheckDouble></span>
+                </ListGroupItem>
+            ))}
+        </ListGroup>
+    )
+}
+
+export default Todos;
